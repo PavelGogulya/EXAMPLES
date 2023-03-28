925a4b5cefca4db1dbac1e1e5d3b4a633bd9ae06
PavelGogulya <PavelGogulya@example.com>
2023-03-28 20:00:49 +0300

    Добавили пример Example 004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿// int numberA = 12;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB); // Операция деления. Поскольку числа натуральные (целые) то программа отсекает 0.4 десятых
+
+double numberA = 14;
+double numberB = 5;
+Console.WriteLine(numberA / numberB); // оператор double позволяет работать с вещественными числами
