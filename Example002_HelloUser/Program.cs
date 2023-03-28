1df528345ab8f117513e73b1a516c888f8c79899
PavelGogulya <PavelGogulya@example.com>
2023-03-28 19:41:38 +0300

    Добавили пример Example 002_Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("Введите ваше имя:"); вывод на НОВОЙ строке
+// string username = Console.ReadLine (); ввод (чтение) данных
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+Console.Write("Введите ваше имя:"); // вывод данных
+string username = Console.ReadLine ();
+Console.Write("Привет, ");
+Console.Write(username);
