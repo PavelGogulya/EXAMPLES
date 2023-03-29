507a52b8be7338e4a2c3a0bcf4cdd130c4365e97
PavelGogulya <PavelGogulya@example.com>
2023-03-29 07:56:11 +0300

    Добавили генератор случайных чисел.add random

--- Example003_/Program.cs
+++ Example003_/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 31;
-int numberB = 50;
+﻿int numberA = new Random().Next(1,10); // т.е интервал с числами 1,2,3,4,5,6,7,8,9, потому что это ПОЛУ интервал и последнее число берется на единицу меньше
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine (numberB);
 int Sum = numberA + numberB;
 Console.WriteLine(Sum);
     
